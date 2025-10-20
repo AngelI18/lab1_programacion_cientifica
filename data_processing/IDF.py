62ce1883b346ff1fa495d9817ab7ae0d85b46251
AngelI18 <angelcastilloiriarte13@gmail.com>
2025-10-20 10:59:06 -0300

    correacción, ajuste de TF e IDF

--- data_processing/IDF.py
+++ data_processing/IDF.py
@@ -1,13 +1,17 @@
 import math
-__all__ = ['CalculoIDF']
+__all__ = ['IDF_corpus']
 
-def CalculoIDF(corpus):
+def IDF_corpus(corpus):
+    if not corpus or all(isinstance(word,str) for word in corpus):
+        print('IDF no es calculable, no hay un corpus de documentos')
+        return {}
+
     num_doc = len(corpus)
     doc_frec = {}
     for doc in corpus:
         words_unique = set(doc)
         for word in words_unique:
-            doc_frec[word] +=1
+            doc_frec[word] = doc_frec.get(word,0) + 1
     
     inv_doc_frec = {word : math.log(num_doc/(1+doc_count)) for word, doc_count in doc_frec.items()}
     return inv_doc_frec
--- data_processing/TF.py
+++ data_processing/TF.py
@@ -1,9 +1,24 @@
-from collections import Counter
+__all__ = ['TF_corpus','TF_doc']
 
-__all__ = ['TF']
-
-def TF(doc):
+def TF_doc(doc):
+    if not doc:
+        return {}
     length = len(doc)
-    word_counter = Counter(doc)
-    tf = {word: (count/length) for word, count in word_counter.items()}
-    return tf
+    word_counter = {}
+    for word in doc:
+        word_counter[word] = word_counter.get(word,0) + 1
+    return {word: (count/length) for word, count in word_counter.items()}
+
+def TF_corpus(corpus):
+    list_doc_tf = []
+    count = 1
+    for doc in corpus:
+        tf = TF_doc(doc)
+        if tf:
+            list_doc_tf.append(tf)
+        else:
+            print(f'doc_{count}_normalizado vacio')
+        count +=1
+
+    return list_doc_tf
+    
